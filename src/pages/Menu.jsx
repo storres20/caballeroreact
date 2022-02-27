2b6c250c0f90030c3f8873817c5c39a2b4fe2447
storres20 <sanchotorres777@gmail.com>
2022-02-27 14:00:10 -0500

    prueba con API en pagina MENU

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
 export default function Menu() {
+  const url = "https://jsonplaceholder.typicode.com/todos"
+  
+  const [todos, setTodos] = useState() //hook
+  
+  const fetchApi = async () => {
+    const response = await fetch(url)
+    const responseJSON = await response.json()
+    setTodos(responseJSON)
+  }
+  
+  useEffect(() => {
+    fetchApi()
+  }, [])
+  
+
   return (
     <div>
       <h1>Menu</h1>
+      {/* Vamos a renderizar el TODO */}
+      <ul>
+        { !todos ? 'Cargando ...' : 
+          todos.map((todo, index) => {
+            return <li key={index}>{todo.title} {todo.completed ? '✅':'❌'}</li>
+          })
+        }
+      </ul>
     </div>
 
   )
